55ab3b30583c260161f64cdffa6c9f9ab8324c5b
KevinMokoro <klosvoke1@gmail.com>
2019-01-22 06:55:06 +0300

    add http user convert.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from "@angular/forms";
+import {HttpClientModule} from '@angular/common/http'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -17,7 +18,9 @@ import { UserFormComponent } from './user-form/user-form.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
+
 
   ],
   providers: [],
