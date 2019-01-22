436027d7fa7b3dd6bf716ec62d9c9eac66aaa807
KevinMokoro <klosvoke1@gmail.com>
2019-01-22 08:21:32 +0300

    add the avatar_url.

--- src/app/user-class/user.ts
+++ src/app/user-class/user.ts
@@ -1,3 +1,3 @@
 export class User {
-  constructor(public login:string,public repos:string,public followers:number,public following:number){}
+  constructor(public login:string,public repos:string,public followers:number,public following:number,public avatar_url:string){}
 }
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -21,10 +21,11 @@ export class UserComponent implements OnInit {
         login:string;
         repos:string;
         followers:number;
-        following:number
+        following:number;
+        avatar_url:string
     }
      this.http.get<ApiResponse>("https://api.github.com/users/KevinMokoro?access_token=ca726da5807d7a2a7e5babf61aebfd7a2611082f").subscribe(data=>{
-    this.user=new User(data.login,data.repos,data.followers,data.following)
+    this.user=new User(data.login,data.repos,data.followers,data.following,data.avatar_url)
       })
   }
 
